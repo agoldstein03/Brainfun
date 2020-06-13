ab7d8080117d45afbae554ef2a73e57b22e8446e
agoldstein03 <38170312+agoldstein03@users.noreply.github.com>
2020-06-13 01:00:49 -0400

    First attempts at adding the editor

--- src/components/Console.vue
+++ src/components/Console.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="container md-elevation-8">
-    ++++++++++++++
+  <div class="consoleContainer md-elevation-8">
     
+    {{ code }}
+
+    <PrismEditor v-model="code" language="bf" :line-numbers="false" class="editor" />
+
     <Play id="play"/>
     <Submit id="submit"/>
   </div>
@@ -11,30 +14,45 @@
 
 import Play from "./Play.vue";
 import Submit from "./Submit.vue";
+import PrismEditor from 'vue-prism-editor';
+import "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
 
 export default {
   name: "Console",
   components: {
     Play,
-    Submit
+    Submit,
+    PrismEditor
   },
   data: function() {
     return {
-      text: "",
+      code: ""
     };
   },
 };
 </script>
 
-<style lang="scss" scoped>
-.container {
+<style lang="scss"> // Careful! THIS IS NOT SCOPED
+/*
+.editor {
+  height: 100%;
+  & > 
+}*/
+
+.prism-editor__code {
+  margin: 0 !important;
+}
+
+.consoleContainer {
   //width: 50vw;
   //overflow: auto;
   background-color: rgb(69, 69, 69);
   font-size: 32px;
   color: white;
   font-family: "Courier New", Courier, monospace;
-  padding: 16px;
+  //padding: 16px;
+  padding: 0px;
   min-height: 112px;
   height: 100%;
 }
